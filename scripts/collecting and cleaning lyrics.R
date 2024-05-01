e1a0009c42228754b3fd6c2eacd9050a749a9f22
Renee <renee@dyn3235-226.wlan.ic.ac.uk>
2024-05-01 15:39:23 +0100

    reorganising folders and files

---
+++ scripts/collecting and cleaning lyrics.R
@@ -0,0 +1,2 @@
+# Using the beyonce lyric data that was found in rfo
+beyonce_lyrics <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-09-29/beyonce_lyrics.csv')
